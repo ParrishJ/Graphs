6eb6cb1f18b6034f5a7e10ccc511faf7bc36db33
jarparrish <jarparrish@gmail.com>
2020-11-20 18:58:29 -0700

    basic traversal implemented

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -29,28 +29,39 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+
+
 #################################################################################################################################################################
 def traverse_rooms(player, world):
     visited_rooms = set()
     traversal_graph = {}
     prev_room_id = None
+    # possible_direction = ['n', 's', 'e', 'w']
 
     player.current_room = world.starting_room
     visited_rooms.add(player.current_room)
-    #current_exits = player.current_room.get_exits()
-
+    
+        
+#################################################################################################################################################################
+    def get_opposite_direction(prev_direction):
+        if prev_direction == 'n':
+            return 's'
+        if prev_direction == 's':
+            return 'n'
+        if prev_direction == 'w':
+            return 'e'
+        if prev_direction == 'e':
+            return 'w'
+    
     def update_graph(player, traversal_path):
         nonlocal prev_room_id 
         visited_rooms.add(player.current_room)
         current_exits = player.current_room.get_exits()
-
+        
         if len(traversal_path) > 0:
             prev_direction = traversal_path[-1]
 
-        # Updates previous room on graph
-        if prev_room_id != None:
-            if traversal_graph[prev_room_id][prev_direction] == '?':
-                traversal_graph[prev_room_id][prev_direction] = player.current_room.id
+        
 
         #updats current room on graph
         for current_exit in current_exits:
@@ -62,76 +73,109 @@ def traverse_rooms(player, world):
                 else: 
                     traversal_graph[player.current_room.id][current_exit] = '?'
         
+        # Updates previous and current rooms on graph
+        if prev_room_id != None:
+            opposite_of_prev_direction = get_opposite_direction(prev_direction)
+            if traversal_graph[prev_room_id][prev_direction] == '?':
+                traversal_graph[prev_room_id][prev_direction] = player.current_room.id
+            if traversal_graph[player.current_room.id][opposite_of_prev_direction] == '?':
+               traversal_graph[player.current_room.id][opposite_of_prev_direction] = prev_room_id
+        
         prev_room_id = player.current_room.id
-    
-    
-    update_graph(player, traversal_path)
-
-    new_direction = 'n'
-    traversal_path.append(new_direction)
-    player.travel(new_direction)
-    update_graph(player, traversal_path)
-
-    new_direction = 'n'
-    traversal_path.append(new_direction)
-    player.travel(new_direction)
+#########################################################################################################################################
     update_graph(player, traversal_path)
     print(traversal_graph)
-
-    new_direction = 's'
-    traversal_path.append(new_direction)
-    player.travel(new_direction)
-    update_graph(player, traversal_path)
-    print(traversal_graph)
-
-    new_direction = 's'
-    traversal_path.append(new_direction)
-    player.travel(new_direction)
-    update_graph(player, traversal_path)
-    print(traversal_graph)
-
-    
-
-    
-
-    #prev_direction = traversal_path[-1]
-
-    #current_exits = player.current_room.get_exits()
-    
-    #current_room_id = player.current_room.id
-    
-    #traversal_graph[player.current_room.id] = dict()
-
-    """ traversal_graph[player.current_room.id] = dict()
-    for current_exit in current_exits:
-        if traversal_graph[player.current_room.id] == {}:
-            traversal_graph[player.current_room.id] = {current_exit: '?'}
-        else: 
-            traversal_graph[player.current_room.id][current_exit] = '?'
-    print('traversal graph 1 ##############################', traversal_graph)
-
-    prev_room_id = player.current_room.id """
-
-    """ if traversal_graph[prev_room_id] != {}:
-        if traversal_graph[prev_room_id][prev_direction] == '?':
-            traversal_graph[prev_room_id][prev_direction] = player.current_room.id
-
-    for current_exit in current_exits:
-        if traversal_graph[player.current_room.id] == {}:
-            traversal_graph[player.current_room.id] = {current_exit: '?'}
-        else: 
-            traversal_graph[current_room_id][current_exit] = '?' """
-    
-    """ print(f'********************************************************Curr Room {player.current_room}********************************************************')
-    print(f'********************************************************Curr Exits {player.current_room.get_exits()}*******************************************')
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Traveled !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    print(f'********************************************************Curr Room {player.current_room.id}********************************************************')
-    print(f'********************************************************Curr Exits {player.current_room.get_exits()}*******************************************') """
-    
-
-
+    while '?' in traversal_graph[player.current_room.id].values():
+        possible_directions = [direction for direction in traversal_graph[player.current_room.id].keys() if traversal_graph[player.current_room.id][direction] == '?' ]
+        random_direction = random.choice(possible_directions)
+        new_direction = random_direction
+        traversal_path.append(new_direction)
+        player.travel(new_direction)
+        update_graph(player, traversal_path)
+        print(traversal_graph)
 
 traverse_rooms(player, world)
+    
+"""     update_graph(player, traversal_path)
+
+    new_direction = 'n'
+    traversal_path.append(new_direction)
+    player.travel(new_direction)
+    update_graph(player, traversal_path)
+
+    new_direction = 'n'
+    traversal_path.append(new_direction)
+    player.travel(new_direction)
+    update_graph(player, traversal_path)
+    print(traversal_graph)
+
+    new_direction = 's'
+    traversal_path.append(new_direction)
+    player.travel(new_direction)
+    update_graph(player, traversal_path)
+    print(traversal_graph)
+
+    new_direction = 's'
+    traversal_path.append(new_direction)
+    player.travel(new_direction)
+    update_graph(player, traversal_path)
+    print(traversal_graph)
+
+    new_direction = 'n'
+    traversal_path.append(new_direction)
+    player.travel(new_direction)
+    update_graph(player, traversal_path)
+    print(traversal_graph)   """
+
+
+
+
+
+    
+
+    
+
+#prev_direction = traversal_path[-1]
+
+#current_exits = player.current_room.get_exits()
+
+#current_room_id = player.current_room.id
+
+#traversal_graph[player.current_room.id] = dict()
+
+""" 
+traversal_graph[player.current_room.id] = dict()
+for current_exit in current_exits:
+    if traversal_graph[player.current_room.id] == {}:
+        traversal_graph[player.current_room.id] = {current_exit: '?'}
+    else: 
+        traversal_graph[player.current_room.id][current_exit] = '?'
+print('traversal graph 1 ##############################', traversal_graph)
+
+prev_room_id = player.current_room.id 
+
+"""
+
+""" 
+
+if traversal_graph[prev_room_id] != {}:
+    if traversal_graph[prev_room_id][prev_direction] == '?':
+        traversal_graph[prev_room_id][prev_direction] = player.current_room.id
+
+for current_exit in current_exits:
+    if traversal_graph[player.current_room.id] == {}:
+        traversal_graph[player.current_room.id] = {current_exit: '?'}
+    else: 
+        traversal_graph[current_room_id][current_exit] = '?' 
+        
+"""
+    
+    
+    
+
+
+
+
 
 
 ################################################################################################################################################################
@@ -167,4 +211,5 @@ while True:
     elif cmds[0] == "q":
         break
     else:
-        print("I did not understand that command.") """
+        print("I did not understand that command.")
+ """
